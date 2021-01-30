04d709e87c4fd2e99a98cb465c70af9c1185eea4
benjamin <ben.pelau@hotmail.fr>
2021-01-30 19:27:56 +0100

    Add header

---
+++ include/header.inc.php
@@ -0,0 +1,11 @@
+<header class="p-1 d-flex d-row justify-space-around">
+    <h1>Benjamin PELAUDEIX</h1>
+    <div>
+        <nav>
+            <a href="#">Présentation</a>
+            <a href="#">Projets</a>
+            <a href="#">Contact</a>
+            <a href="#">Documents</a>
+        </nav>
+    </div>
+</header>
