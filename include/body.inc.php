160d1f91cf4d4a56a4b342b3e5e302664032e765
benjamin <ben.pelau@hotmail.fr>
2021-01-31 12:36:06 +0100

    Add presentation part

---
+++ include/body.inc.php
@@ -0,0 +1,210 @@
+<body>
+        <div id="home">
+            <div class="typer-style container">
+                <h1>//
+                    <span class="typer" id="main" data-words="Développeur Web, Étudiant, Intégrateur, UX/UI Designer, Graphiste" data-delay="80" data-delete-delay="850" data-colors="white"></span>
+                    <script async src="https://unpkg.com/typer-dot-js@0.1.0/typer.js"></script>
+                </h1>
+            </div>
+        </div>
+        <div id="presentation">
+            <div class="container">
+                <h2>Présentation</h2>
+                <h3>À propos de moi</h3>
+                <p>Bonjour ! Je m'appelle Benjamin PELAUDEIX. J'ai 20 ans et je suis actuellement étudiant en DUT Informatique. J'apprends dans ma formation le développement logiciel, orienté objet ou non, le développement web, côté client et serveur, et je suis aussi sensibilisé à la gestion de projet, la gestion d'entreprise ainsi que la communication au sein des organisations. En dehors de mes études, j'aime approfondir mes connaissances et participer à des cours en ligne via OpenClassroom ou Udemy, les deux plateformes principales que j'utilise. Ce sont ces enseignements en autodidacte qui m'ont amené à aimer l'informatique et la programmation, surtout orientée web.
+                    <br>
+                    Je suis optimiste, enthousiaste, curieux mais surtout travailleur. Je ne compte pas les heures passées à travailler sur un projet. J'ai un grand sens de la détermination et je perfectionniste. Il m'arrive parfois de faire plusieurs maquettes pour trouver celle qui correspond le mieux au projet.</p>
+                <h3>Mes compétences</h3>
+                <h4><i class="fas fa-desktop"></i> Technologies logicielles</h4>
+                <div class="flex">
+                    <div>
+                        <p>C++</p>
+                        <div class="progress">
+                            <div class="progress-bar" id="cpp"></div>
+                        </div>
+                    </div>
+                    <div>
+                        <p>Java</p>
+                        <div class="progress">
+                            <div class="progress-bar" id="java"></div>
+                        </div>
+                    </div>
+                </div>
+                <h4><i class="fas fa-globe"></i> Technologies web</h4>
+                <div class="flex">
+                    <div>
+                        <p>HTML 5</p>
+                        <div class="progress">
+                            <div class="progress-bar" id="html"></div>
+                        </div>
+                    </div>
+                    <div>
+                        <p>CSS3</p>
+                        <div class="progress">
+                            <div class="progress-bar" id="css"></div>
+                        </div>
+                    </div>
+                    <div>
+                        <p>JavaScript</p>
+                        <div class="progress">
+                            <div class="progress-bar" id="js"></div>
+                        </div>
+                    </div>
+                    <div>
+                        <p>Typescript</p>
+                        <div class="progress">
+                            <div class="progress-bar" id="ts"></div>
+                        </div>
+                    </div>
+                    <div>
+                        <p>NodeJS</p>
+                        <div class="progress">
+                            <div class="progress-bar" id="node"></div>
+                        </div>
+                    </div>
+                    <div>
+                        <p>PHP</p>
+                        <div class="progress">
+                            <div class="progress-bar" id="php"></div>
+                        </div>
+                    </div>
+                </div>
+                <h4><i class="fas fa-database"></i> Bases de données</h4>
+                <div class="flex">
+                    <div>
+                        <p>Microsoft SQL</p>
+                        <div class="progress">
+                            <div class="progress-bar" id="mssql"></div>
+                        </div>
+                    </div>
+                    <div>
+                        <p>MySQL</p>
+                        <div class="progress">
+                            <div class="progress-bar" id="mysql"></div>
+                        </div>
+                    </div>
+                </div>
+                <h4><i class="fas fa-tools"></i> Outils</h4>
+                <div class="flex">
+                    <div>
+                        <p>Microsoft Visual Studio</p>
+                        <div class="progress">
+                            <div class="progress-bar" id="vs"></div>
+                        </div>
+                    </div>
+                    <div>
+                        <p>Suite JetBrains</p>
+                        <div class="progress">
+                            <div class="progress-bar" id="jetbrains"></div>
+                        </div>
+                    </div>
+                    <div>
+                        <p>Suite Office</p>
+                        <div class="progress">
+                            <div class="progress-bar" id="office"></div>
+                        </div>
+                    </div>
+                    <div>
+                        <p>Suite Google</p>
+                        <div class="progress">
+                            <div class="progress-bar" id="google"></div>
+                        </div>
+                    </div>
+                    <div>
+                        <p>Adobe Photoshop</p>
+                        <div class="progress">
+                            <div class="progress-bar" id="psd"></div>
+                        </div>
+                    </div>
+                    <div>
+                        <p>Adobe XD</p>
+                        <div class="progress">
+                            <div class="progress-bar" id="xd"></div>
+                        </div>
+                    </div>
+                </div>
+                <h4><i class="fas fa-robot"></i> Systèmes d'exploitation</h4>
+                <div class="flex">
+                    <div>
+                        <p>Windows</p>
+                        <div class="progress">
+                            <div class="progress-bar" id="windows"></div>
+                        </div>
+                    </div>
+                    <div>
+                        <p>MacOS</p>
+                        <div class="progress">
+                            <div class="progress-bar" id="macos"></div>
+                        </div>
+                    </div>
+                    <div>
+                        <p>Linux</p>
+                        <div class="progress">
+                            <div class="progress-bar" id="linux"></div>
+                        </div>
+                    </div>
+                </div>
+                <h3>Mes expériences</h3>
+                <h4><i class="fas fa-graduation-cap"></i> Universitaires</h4>
+                <div class="flex">
+                    <div>
+                        <p>Graphiste (Septembre 2020-)</p>
+                        <p>OEIL, Limoges (87)</p>
+                        <p>Missions :</p>
+                        <ul>
+                            <li>Création d'un logotype pour l'année 2020-2021</li>
+                            <li>Création de visuels pour les communications</li>
+                        </ul>
+                    </div>
+                    <div>
+                        <p>Responsable graphique (Septembre 2020-)</p>
+                        <p>Log_Mag, Limoges (87)</p>
+                        <p>Missions : </p>
+                        <ul>
+                            <li>Création d'une charte graphique</li>
+                            <li>Mise en page des éditions au format PDF</li>
+                        </ul>
+                    </div>
+                </div>
+                <h4><i class="fas fa-briefcase"></i> Professionnelles</h4>
+                <div class="flex">
+                    <div>
+                        <p>Emploi saisonnier (Juin 2017-Septembre 2017)</p>
+                        <p>Best Courtage - Limoges (87)</p>
+                        <p>Missions : </p>
+                        <ul>
+                            <li>Création de visuels pour les communications</li>
+                            <li>Gestion des réseaux sociaux</li>
+                        </ul>
+                    </div>
+                </div>
+                <h4><i class="fas fa-user"></i> Personnelles</h4>
+                <div class="flex">
+                    <div>
+                        <p>Arbitre (Septembre 2017-)</p>
+                        <p>Ligue Nouvelle-Aquitaine de Basket-Ball</p>
+                        <p>Missions : </p>
+                        <ul>
+                            <li>Arbitre officiel au niveau pré-national séniors et fédéral jeunes</li>
+                            <li>Formateur des arbitres au sein de mon club</li>
+                            <li>Formation continue avec la ligue Nouvelle-Aquitaine</li>
+                        </ul>
+                    </div>
+                    <div>
+                        <p>Responsable Développement (Janvier 2019-)</p>
+                        <p>Loud Esport</p>
+                        <p>Missions : </p>
+                        <ul>
+                            <li>Membre exécutif de l'association</li>
+                            <li>Aide à la prise de décision</li>
+                            <li>Management d'une section Trackmania</li>
+                            <li>Gestion des réseaux sociaux</li>
+                            <li>Réalisation d'un site vitrine</li>
+                            <li>Créateur de contenu</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <a href="#top" id="back-top"><i class="fas fa-arrow-up"></i></a>
+</body>
--- include/header.inc.php
+++ include/header.inc.php
@@ -1,7 +1,7 @@
-<header>
+<header id="top">
     <h1>Benjamin PELAUDEIX</h1>
     <nav>
-        <a href="#">Présentation</a>
+        <a href="#presentation">Présentation</a>
         <a href="#">Projets</a>
         <a href="#">Contact</a>
         <a href="#"><i class="fas fa-lock"></i></a>
