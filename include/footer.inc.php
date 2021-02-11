e7c59de2dcc10416ecd7c69edae67e2cedef7ab1
benjamin <ben.pelau@hotmail.fr>
2021-02-11 12:09:13 +0100

    Add legal mentions

--- include/footer.inc.php
+++ include/footer.inc.php
@@ -1,3 +1,3 @@
 <footer>
-    <p>© Benjamin PELAUDEIX - Tous droits réservés - <span id="year"></span> - <a href="https://www.github.com/profile/Benjamin-Pelaudeix"><i class="fab fa-github"></i></a></p>
+    <p>© Benjamin PELAUDEIX - Tous droits réservés - <span id="year"></span> - <a href="./documents/mentions-legales.pdf">Mentions légales</a> - <a href="https://www.github.com/profile/Benjamin-Pelaudeix"><i class="fab fa-github"></i></a></p>
 </footer>
