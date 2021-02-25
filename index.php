f68ed90d380b2ca88c7299de4a64dc172d0f1362
Benjamin PELAUDEIX <34833521+Benjamin-Pelaudeix@users.noreply.github.com>
2021-02-26 00:03:55 +0100

    add meta description for SEO

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Bienvenue sur mon portfolio ! Venez découvrir mon compétences, mes projets pour peut-être que nous collaborions">
     <title>Benjamin PELAUDEIX</title>
     <!-- stylesheet -->
     <link rel="stylesheet" href="css/style.css">
